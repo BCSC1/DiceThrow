c421e916fcb66ac014aa21adbaedefc7990d94aa
BCSC1 <156947295+BCSC1@users.noreply.github.com>
2024-10-17 15:12:53 -0400

    Add fragment to change number of sides

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -7,10 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import kotlin.random.Random
+const val DIESIDE = "sidenumber"
 
 class DieFragment : Fragment() {
 
-    val DIESIDE = "sidenumber"
 
     lateinit var dieTextView: TextView
 
@@ -46,4 +46,18 @@ class DieFragment : Fragment() {
     fun throwDie() {
         dieTextView.text = (Random.nextInt(dieSides) + 1).toString()
     }
+
+
+    companion object {
+
+        fun newInstance(sides: Int)=
+            DieFragment().apply{
+            arguments = Bundle().apply{
+                putInt(DIESIDE, sides)
+            }
+        }
+    }
+
+
+
 }
--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -16,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
             (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
         }
+
+
+        val fragment1 = DieFragment.newInstance(20)
+        supportFragmentManager.beginTransaction().add(R.id.dieContainer, fragment1).commit()
+
     }
 }
